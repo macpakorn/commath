ac30b3ef2b331bdb19b92b179082f27894df8294
macpakorn <pakorn.sa.61@ubu.ac.th>
2020-09-02 11:05:09 +0700

    Updates

---
+++ precache-manifest.b56bd0d3375036b6f996699ba634f51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b136d4c99c911899445e62ff4ecdb44c",
+    "url": "/commath/index.html"
+  },
+  {
+    "revision": "371613c9342dada57443",
+    "url": "/commath/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "4cfc160bb3d13546e2db",
+    "url": "/commath/static/js/2.c5010d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath/static/js/2.c5010d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371613c9342dada57443",
+    "url": "/commath/static/js/main.9ad3ce33.chunk.js"
+  },
+  {
+    "revision": "b38a0d82b0f04e9f8629",
+    "url": "/commath/static/js/runtime-main.dd2f9e9c.js"
+  }
+]);
